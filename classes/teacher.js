1fd244ee345b7e0c2472535a903858e6f35ecf8e
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-07-15 14:22:13 -0400

    Solve teacher.js

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,21 @@
 const Person = require('./person');
 
-// Your code here
+// class "Teacher" that is a subclass of Person
+class Teacher extends Person {
+  // constructor function; input: firstName, lastName, age, subject, yearsOfExperience
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName); // super function; input: firstName, lastName, age
+    this.subject = subject; // subject property
+    this.yearsOfExperience = yearsOfExperience; // yearsOfExperience property
+  }
+  
+  // static method "combinedYearsOfExperience"; input: array of Teacher instances; output: returns sum of input array
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((sum, teacher) => {
+      return sum + teacher.yearsOfExperience;
+    }, 0);
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
